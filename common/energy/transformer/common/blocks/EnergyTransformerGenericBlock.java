fa14e63fe63bdf43a98cc7a1f45ff2a115372dc2
utybo <utybo7@me.com>
2014-05-17 21:38:37 +0200

    Added generic blocks class
    This will be used in the future for any block that does not have a
    special purpose

---
+++ common/energy/transformer/common/blocks/EnergyTransformerGenericBlock.java
@@ -0,0 +1,15 @@
+package energy.transformer.common.blocks;
+
+import energy.transformer.common.EnergyTransformer;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class EnergyTransformerGenericBlock extends Block {
+
+	protected EnergyTransformerGenericBlock(Material material, String blockName, String textureName) {
+		super(material);
+		this.setBlockName(blockName);
+		this.setBlockTextureName(EnergyTransformer.MODID + ":" + textureName);
+	}
+
+}
