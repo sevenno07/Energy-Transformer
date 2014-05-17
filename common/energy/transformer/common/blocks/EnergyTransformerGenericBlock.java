792bdf04c05fe7b5dd522887daa79f8b5c544bcc
utybo <utybo7@me.com>
2014-05-18 01:17:33 +0200

    Revert "Added another constructor"
    This reverts commit 46f15e4e9472c7adc8495472221373300185ec2b.

--- common/energy/transformer/common/blocks/EnergyTransformerGenericBlock.java
+++ common/energy/transformer/common/blocks/EnergyTransformerGenericBlock.java
@@ -11,12 +11,5 @@ public class EnergyTransformerGenericBlock extends Block {
 		this.setBlockName(blockName);
 		this.setBlockTextureName(EnergyTransformer.MODID.toLowerCase() + ":" + textureName);
 	}
-	protected EnergyTransformerGenericBlock(Material material, String blockName, String textureName, float miningResistance, int lightLevel) {
-		super(material);
-		this.setBlockName(blockName);
-		this.setBlockTextureName(EnergyTransformer.MODID.toLowerCase() + ":" + textureName);
-		this.setHardness(miningResistance);
-		this.setLightLevel(lightLevel);
-	}
 
 }
