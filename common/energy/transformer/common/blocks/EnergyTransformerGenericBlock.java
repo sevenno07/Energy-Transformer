46f15e4e9472c7adc8495472221373300185ec2b
utybo <utybo7@me.com>
2014-05-17 23:33:23 +0200

    Added another constructor
    Detailed block declaration. **WILL BE USED IN THE FUTURE, THIS IS NOT
    IMPORTANT FOR THE MOMENT**

--- common/energy/transformer/common/blocks/EnergyTransformerGenericBlock.java
+++ common/energy/transformer/common/blocks/EnergyTransformerGenericBlock.java
@@ -11,5 +11,12 @@ public class EnergyTransformerGenericBlock extends Block {
 		this.setBlockName(blockName);
 		this.setBlockTextureName(EnergyTransformer.MODID.toLowerCase() + ":" + textureName);
 	}
+	protected EnergyTransformerGenericBlock(Material material, String blockName, String textureName, float miningResistance, int lightLevel) {
+		super(material);
+		this.setBlockName(blockName);
+		this.setBlockTextureName(EnergyTransformer.MODID.toLowerCase() + ":" + textureName);
+		this.setHardness(miningResistance);
+		this.setLightLevel(lightLevel);
+	}
 
 }
