d75de844778b36814b7fe15629abede3e801b2d2
utybo <utybo7@me.com>
2014-05-17 21:47:45 +0200

    Added generic items class
    See commit fa14e63fe63bdf43a98cc7a1f45ff2a115372dc2 for detailed
    explanations

---
+++ common/energy/transformer/common/items/EnergyTransformerGenericItem.java
@@ -0,0 +1,12 @@
+package energy.transformer.common.items;
+
+import net.minecraft.item.Item;
+
+public class EnergyTransformerGenericItem extends Item {
+
+	protected EnergyTransformerGenericItem(String itemName, String textureName) {
+		this.setUnlocalizedName(itemName);
+		this.setTextureName(textureName);
+	}
+
+}
