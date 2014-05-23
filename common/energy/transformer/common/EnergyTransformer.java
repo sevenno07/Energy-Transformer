8f90b3e2ff0bd9f65a2857464924ea60f8f77ee5
utybo <utybo7@me.com>
2014-05-23 19:01:14 +0200

    Added logger + Formatted classes AGAIN
    Added Item loading, not important

--- common/energy/transformer/common/EnergyTransformer.java
+++ common/energy/transformer/common/EnergyTransformer.java
@@ -1,5 +1,6 @@
 package energy.transformer.common;
 
+import org.apache.logging.log4j.*;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -8,33 +9,38 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import energy.transformer.common.blocks.EnergyBlockList;
+import energy.transformer.common.items.EnergyItemList;
 import energy.transformer.proxy.EnergyCommonProxy;
 
 @Mod(modid = EnergyTransformer.MODID, name = "Energy-Transformer", version = "@VERSION@")
 public class EnergyTransformer
 {
+	public static final Logger LOGGER = LogManager.getLogger("Energy-Transformer");
 	public static final String MODID = "Energy_Transformer";
 	@SidedProxy(clientSide = "energy.transformer.proxy.EnergyClientProxy", serverSide = "energy.transformer.proxy.EnergyCommonProxy")
 	public static EnergyCommonProxy proxy;
-	
+
 	@Instance("Energy_Transformer")
 	public static EnergyTransformer instance;
-	
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
+		LOGGER.info("Loading blocks");
 		EnergyBlockList.loadBlocks();
+		LOGGER.info("Loading items");
+		EnergyItemList.loadItems();
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		
+
 	}
-	
+
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
-		
+
 	}
 }
