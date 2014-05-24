57cd15c7f24009b32986019b74a5e86a922230c0
utybo <utybo7@me.com>
2014-05-24 12:19:27 +0200

    (WIP) Added EPC values list

---
+++ common/energy/transformer/common/epc/EnergyEPCValuesList.java
@@ -0,0 +1,83 @@
+package energy.transformer.common.epc;
+
+import net.minecraft.init.Blocks;
+
+public class EnergyEPCValuesList
+{
+	public static void loadEPCValues()
+	{
+		loadVanillaEPCValues();
+	}
+
+	private static void loadVanillaEPCValues()
+	{
+		//Blocks
+		EPCDatabase.setEPCValue(Blocks.stone, 4);
+		EPCDatabase.setEPCValue(Blocks.grass, 4);
+		EPCDatabase.setEPCValue(Blocks.dirt, 1);
+		EPCDatabase.setEPCValue(Blocks.cobblestone, 1);
+		EPCDatabase.setEPCValue(Blocks.planks, 2);
+		EPCDatabase.setEPCValue(Blocks.sapling, 4);
+		EPCDatabase.setEPCValue(Blocks.sand, 2);
+		EPCDatabase.setEPCValue(Blocks.gravel, 2);
+		EPCDatabase.setEPCValue(Blocks.gold_ore, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.iron_ore, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.coal_ore, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.log, 8);
+		EPCDatabase.setEPCValue(Blocks.log2, 8);
+		EPCDatabase.setEPCValue(Blocks.leaves, 4);
+		EPCDatabase.setEPCValue(Blocks.leaves2, 4);
+		EPCDatabase.setEPCValue(Blocks.glass, 8);
+		EPCDatabase.setEPCValue(Blocks.lapis_ore, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.lapis_block, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.dispenser, 2); //TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.sandstone, 4);
+		EPCDatabase.setEPCValue(Blocks.noteblock, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.golden_rail, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.web, 8);
+		EPCDatabase.setEPCValue(Blocks.tallgrass, 6);
+		EPCDatabase.setEPCValue(Blocks.deadbush, 8);
+		EPCDatabase.setEPCValue(Blocks.wool, 16);
+		EPCDatabase.setEPCValue(Blocks.yellow_flower, 16);
+		EPCDatabase.setEPCValue(Blocks.red_flower, 16);
+		EPCDatabase.setEPCValue(Blocks.brown_mushroom, 16);
+		EPCDatabase.setEPCValue(Blocks.red_mushroom, 16);
+		EPCDatabase.setEPCValue(Blocks.gold_block, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.iron_block, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.stone_slab, 1);
+		EPCDatabase.setEPCValue(Blocks.brick_block, 16);
+		EPCDatabase.setEPCValue(Blocks.tnt, 32);
+		EPCDatabase.setEPCValue(Blocks.bookshelf, 64);
+		EPCDatabase.setEPCValue(Blocks.mossy_cobblestone, 3);
+		EPCDatabase.setEPCValue(Blocks.obsidian, 128);
+		EPCDatabase.setEPCValue(Blocks.torch, 0);
+		EPCDatabase.setEPCValue(Blocks.mob_spawner, 256);
+		EPCDatabase.setEPCValue(Blocks.oak_stairs, 8);
+		EPCDatabase.setEPCValue(Blocks.chest, 16);
+		EPCDatabase.setEPCValue(Blocks.diamond_ore, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.diamond_block, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.crafting_table, 8);
+		EPCDatabase.setEPCValue(Blocks.furnace, 8);
+		EPCDatabase.setEPCValue(Blocks.lever, 2);
+		EPCDatabase.setEPCValue(Blocks.stone_pressure_plate, 12);
+		EPCDatabase.setEPCValue(Blocks.wooden_pressure_plate, 6);
+		EPCDatabase.setEPCValue(Blocks.redstone_ore, 2);
+		EPCDatabase.setEPCValue(Blocks.stone_button, 4);
+		EPCDatabase.setEPCValue(Blocks.ice, 16);
+		EPCDatabase.setEPCValue(Blocks.snow, 4);
+		EPCDatabase.setEPCValue(Blocks.cactus, 8);
+		EPCDatabase.setEPCValue(Blocks.clay, 4);
+		EPCDatabase.setEPCValue(Blocks.jukebox, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.pumpkin, 32);
+		EPCDatabase.setEPCValue(Blocks.netherrack, 8);
+		EPCDatabase.setEPCValue(Blocks.soul_sand, 32);
+		EPCDatabase.setEPCValue(Blocks.glowstone, 64);
+		EPCDatabase.setEPCValue(Blocks.lit_pumpkin, 40);
+		EPCDatabase.setEPCValue(Blocks.trapdoor, 3);
+		EPCDatabase.setEPCValue(Blocks.stonebrick, 16);
+		EPCDatabase.setEPCValue(Blocks.iron_bars, 2); // TODO Replace this value
+		EPCDatabase.setEPCValue(Blocks.glass_pane, 2);
+		//TODO Finish the list
+		
+	}
+}
