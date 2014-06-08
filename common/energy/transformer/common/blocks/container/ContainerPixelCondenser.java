7267a555529192f22fcafe45bef3eac888663d82
Superloup10 <Superloup10@gmail.com>
2014-06-08 03:22:00 +0200

    fix Shift-Click

--- common/energy/transformer/common/blocks/container/ContainerPixelCondenser.java
+++ common/energy/transformer/common/blocks/container/ContainerPixelCondenser.java
@@ -4,12 +4,13 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import energy.transformer.common.tileentity.TileEntityPixelCondenser;
 
 public class ContainerPixelCondenser extends Container
 {
 	private TileEntityPixelCondenser tileEntityPixelCondenser;
-	
+
 	public ContainerPixelCondenser(InventoryPlayer inventory, TileEntityPixelCondenser te)
 	{
 		this.tileEntityPixelCondenser = te;
@@ -18,12 +19,12 @@ public class ContainerPixelCondenser extends Container
 		{
 			this.addSlotToContainer(new Slot(te, j * 9 + 16, 35 + j * 18, 26));
 		}
-		
+
 		for(int i = 0; i < 3; i++)
 		{
 			this.addSlotToContainer(new Slot(te, i * 9 + 30, 51 + i * 20, 65));
 		}
-		
+
 		for(int i = 0; i < 5; i++)
 		{
 			for(int j = 0; j < 3; j++)
@@ -31,9 +32,9 @@ public class ContainerPixelCondenser extends Container
 				this.addSlotToContainer(new Slot(te, i + j * 10, 35 + i * 18, 99 + j * 18));
 			}
 		}
-		
+
 		this.addSlotToContainer(new Slot(te, 40, 146, 94));
-		
+
 		this.bindPlayerInventory(inventory);
 	}
 
@@ -47,7 +48,7 @@ public class ContainerPixelCondenser extends Container
 				this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 47 + j * 18, 103 + i * 18 + 62));
 			}
 		}
-		
+
 		for(i = 0; i < 9; i++)
 		{
 			this.addSlotToContainer(new Slot(inventory, i, 47 + i * 18, 223));
@@ -58,5 +59,40 @@ public class ContainerPixelCondenser extends Container
 	public boolean canInteractWith(EntityPlayer player)
 	{
 		return tileEntityPixelCondenser.isUseableByPlayer(player);
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotId)
+	{
+		ItemStack itemstack = null;
+		Slot slot = (Slot)this.inventorySlots.get(slotId);
+
+		if(slot != null && slot.getHasStack())
+		{
+			ItemStack itemstack1 = slot.getStack();
+			itemstack = itemstack1.copy();
+
+			if(slotId < 9)
+			{
+				if(!this.mergeItemStack(itemstack1, 9, this.inventorySlots.size(), true))
+				{
+					return null;
+				}
+			}
+			else if(!this.mergeItemStack(itemstack1, 0, 9, false))
+			{
+				return null;
+			}
+
+			if(itemstack1.stackSize == 0)
+			{
+				slot.putStack((ItemStack)null);
+			}
+			else
+			{
+				slot.onSlotChanged();
+			}
+		}
+		return itemstack;
 	}
 }
--- common/energy/transformer/common/gui/GuiPixelCondenser.java
+++ common/energy/transformer/common/gui/GuiPixelCondenser.java
@@ -107,15 +107,15 @@ public class GuiPixelCondenser extends GuiContainer
 		this.textField.drawTextBox();
 	}
 
-    private int inventoryRows = 0;
-	
+	private int inventoryRows = 0;
+
 	@Override
-    protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
-    {
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(pixelCondenserGuiTexture);
-        int k = (this.width - this.xSize) / 2;
-        int l = (this.height - this.ySize) / 2;
-        this.drawTexturedModalRect(k + 27, l + 10, 0, 0, 215, 245);
-    }
+	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
+	{
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.getTextureManager().bindTexture(pixelCondenserGuiTexture);
+		int k = (this.width - this.xSize) / 2;
+		int l = (this.height - this.ySize) / 2;
+		this.drawTexturedModalRect(k + 27, l + 10, 0, 0, 215, 245);
+	}
 }
